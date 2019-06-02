9a29273f6522098de1a4bab094873b5ce766e539
Richard Wotzlaw <r.wotzlaw@gmail.com>
2019-06-02 10:21:22 +0200

    chessboard escape game

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-const textfeld = document.getElementById('textfeld');
-const knopf = document.getElementById('knopf');
-const liste = document.getElementById('liste');
+const elementMap = [];
 
-knopf.addEventListener('click', clickHandler);
+constructBoard(30, 30);
 
-function clickHandler() {
-  const neuesElement = document.createElement('li');
-  const inhalt = document.createTextNode(textfeld.value);
-  neuesElement.appendChild(inhalt);
+function constructBoard(cols, rows) {
+  const boardTable = document.getElementById('board');
   
-  liste.appendChild(neuesElement);
-  
-  textfeld.value = '';
+  for (let r = 0; r < rows; r++) {
+    const row = document.createElement('tr');
+    const elementRow = [];
+    
+    for (let c = 0; c < cols; c++) {
+      const cell = document.createElement('td');
+      cell.dataset.column = c;
+      cell.dataset.row = r;
+      cell.addEventListener('click', clickHandler);
+      
+      if ((r === 0 && c < 2) || (r === 1 && c === 0)) {
+        cell.className = 'has-piece';
+      } else {
+        cell.className = '';
+      }
+      
+      row.appendChild(cell);
+      elementRow.push(cell);
+    }
+    
+    boardTable.appendChild(row);
+    elementMap.push(elementRow);
+  }
 }
 
-const haus = {
-  baujahr: 1900,
-  stockwerke: 5,
-  strasse: 'Soldiner Str',
-  farbe: 'blau',
-  einstuerzen: function() {
-    console.log('krawumm');
+function clickHandler(e) {
+  const clickedCell = e.target;
+  const clickedRow = parseInt(clickedCell.dataset.row, 10);
+  const clickedCol = parseInt(clickedCell.dataset.column, 10);
+  const rightPropagationCell = elementMap[clickedRow][clickedCol + 1];
+  const bottomPropagationCell = elementMap[clickedRow + 1][clickedCol];
+  
+  if (!rightPropagationCell || !bottomPropagationCell) {
+    console.log('Out of bounds!');
+    return;
   }
-};
-
-haus.einstuerzen();
+  
+  if (
+    rightPropagationCell.classList.contains('has-piece') ||
+    bottomPropagationCell.classList.contains('has-piece') ||
+    !clickedCell.classList.contains('has-piece')
+  ) {
+    console.log('Illegal move!');
+    return;
+  }
+  
+  console.log(`Clicked on row ${clickedRow}, column ${clickedCol}`);
+  clickedCell.classList.remove('has-piece');
+  rightPropagationCell.classList.add('has-piece');
+  bottomPropagationCell.classList.add('has-piece');
+}
